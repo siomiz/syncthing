4df1a700ae8c6a1c0b24041080ba1202da1031b1
Tomohisa Kusano <siomiz@gmail.com>
2015-07-07 16:26:53 -0700

    handle legacy WebUI config

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 /opt/syncthing/syncthing -generate="/opt/syncthing/config.d"
 
-/bin/sed -ri 's/127\.0\.0\.1:8080/0.0.0.0:8080/;' /opt/syncthing/config.d/config.xml
+/bin/sed -ri 's/0\.0\.0\.0:8080/0.0.0.0:8384/;' /opt/syncthing/config.d/config.xml
 /bin/sed -ri 's/127\.0\.0\.1:8384/0.0.0.0:8384/;' /opt/syncthing/config.d/config.xml
 
 exec "$@"
